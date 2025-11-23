6ab8c936d819acdcde0f51fc922ef7c33f354354
IgorJoaquimn <IgorJoaquimn>
2025-11-22 23:32:02 -0300

    Add Oak Tree tileset for use in the game

---
+++ assets/tiled/tilesets/Oak_Tree.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Oak_Tree" tilewidth="64" tileheight="80" tilecount="1" columns="1">
+ <tileoffset x="-16" y="16"/>
+ <image source="../../third_party/Cute_Fantasy_Free/Outdoor decoration/Oak_Tree.png" width="64" height="80"/>
+</tileset>
